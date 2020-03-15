97a6609a90ef6261b078dae5edb921b7cbf24740
Fili Santillán <fili06604@gmail.com>
2020-03-15 17:22:57 -0600

    :art: Structure complete

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+export const Header = () => {
+  const view = `
+    <div class="Header-main">
+      <div class="Header-logo">
+        <h1>
+          <a href="/">
+            JS Vanilla SPA
+          </a>
+        </h1>
+      </div>
+      <div class="Header-nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </div>
+  `;
+  return view;
+}
